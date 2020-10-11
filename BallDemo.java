3c2ef9ab91492a8c6a1a5ef9a3b065d294a1dcc4
gregbabbert <60162794+gregbabbert@users.noreply.github.com>
2020-10-11 00:37:48 -0400

    make box

--- BallDemo.java
+++ BallDemo.java
@@ -51,4 +51,40 @@ public class BallDemo
             }
         }
     }
+    
+    /**
+     * Simulate two bouncing balls
+     */
+    public void boxBounce()
+    {
+        int ground = 400;   // position of the ground line
+
+        myCanvas.setVisible(true);
+
+        // draw the box
+        myCanvas.setForegroundColor(Color.BLACK);
+        myCanvas.drawLine(50, ground, 550, ground);
+        myCanvas.drawLine(50, 25, 550, 25);
+        myCanvas.drawLine(50, ground, 50, 25);
+        myCanvas.drawLine(550, ground, 550, 25);
+
+        // create and show the balls
+        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+        ball.draw();
+        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
+        ball2.draw();
+
+        // make them bounce
+        boolean finished =  false;
+        while (!finished) {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
+            // stop once ball has travelled a certain distance on x axis
+            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+                finished = true;
+            }
+        }
+    }
+    
 }
